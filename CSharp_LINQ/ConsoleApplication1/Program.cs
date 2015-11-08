860f21bd3243367c37cd82e6c7100c1a874d0109
Chris_Jarvis <clj@ollure.com>
2015-11-08 10:27:28 -0600

    walkthrough completed

--- CSharp_LINQ/ConsoleApplication1/Program.cs
+++ CSharp_LINQ/ConsoleApplication1/Program.cs
@@ -20,44 +20,29 @@ namespace ConsoleApplication1
             //execute the query 
             //could also use var keyword
 
-            /*   
-            ////1st group
-                foreach (Student student in studentQuery)
-                {
-                    Console.WriteLine( "{0}, {1} {2}", student.Last, student.First, student.Scores[0]);
-                }
-                */
+            var studentQuery6 =
+              from student in students
+              let totalScore = student.Scores[0] + student.Scores[1] +
+                  student.Scores[2] + student.Scores[3]
 
-            /*
-            ///studentQuery2
-            var studentQuery2 =
+              select totalScore;
+
+            double averageScore = studentQuery6.Average();
+          //  Console.WriteLine("Class average score = {0}", averageScore);
+
+            var studentQuery8 =
                 from student in students
-                group student by student.Last[0];
+                let x = student.Scores[0] + student.Scores[1] +
+                        student.Scores[2] + student.Scores[3]
+                where x > averageScore
+                select new { id = student.ID, score = x };
 
-            foreach (var StudentGroup in studentQuery2) 
+      
+            foreach (var item in studentQuery8)
             {
-                Console.WriteLine(StudentGroup.Key);
-                foreach (Student student in StudentGroup)
-                {
-                    Console.WriteLine(" {0}, {1}",
-                               student.Last, student.First);
-                }
+                Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
             }
-            */
 
-            var studentQuery3 =
-            from student in students
-            group student by student.Last[0];
-
-            foreach (var groupOfStudents in studentQuery3)
-            {
-                Console.WriteLine(groupOfStudents.Key);
-                foreach (var student in groupOfStudents)
-                {
-                    Console.WriteLine("   {0}, {1}",
-                        student.Last, student.First);
-                }
-            }
         } 
              public class Student
         {
@@ -88,3 +73,95 @@ namespace ConsoleApplication1
     
 
 }
+
+/*   
+        ////1st group
+            foreach (Student student in studentQuery)
+            {
+                Console.WriteLine( "{0}, {1} {2}", student.Last, student.First, student.Scores[0]);
+        }
+        */
+
+            /*
+        ///studentQuery2
+        var studentQuery2 =
+            from student in students
+            group student by student.Last[0];
+
+        foreach (var StudentGroup in studentQuery2) 
+        {
+            Console.WriteLine(StudentGroup.Key);
+            foreach (Student student in StudentGroup)
+            {
+                Console.WriteLine(" {0}, {1}",
+                           student.Last, student.First);
+            }
+        }
+        */
+
+            /*
+            /////////////////////Query 3
+            var studentQuery3 =
+            from student in students
+            group student by student.Last[0];
+
+            foreach (var groupOfStudents in studentQuery3)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+                var studentQuery4 =
+                from student in students
+                group student by student.Last[0] into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+
+            foreach (var groupOfStudents in studentQuery4)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("  {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+            var studentQuery5 =
+                from student in students
+                let totalScore = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+                where totalScore / 4 < student.Scores[0]
+                select student.Last + " " + student.First;
+
+            foreach (string s in studentQuery5)
+            {
+                Console.WriteLine(s);
+            }
+
+                var studentQuery6 =
+                from student in students
+                let totalScore = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+
+                select totalScore;
+
+            double averageScore = studentQuery6.Average();
+            Console.WriteLine("Class average score = {0}", averageScore);
+
+            var studentQuery7 =
+                from student in students
+                where student.Last == "Garcia"
+                select student.First;
+
+            Console.WriteLine("The Garcia in the class are:");
+            foreach (string s in studentQuery7)
+            {
+                Console.WriteLine(s);
+            }
+
+*/
